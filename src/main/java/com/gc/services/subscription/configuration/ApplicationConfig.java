048dbf905f22b7cc4f34026ccf641f4437685a74
pablo <pabnicalt@gmail.com>
2024-08-27 17:35:34 -0300

    several fixes

--- src/main/java/com/gc/services/subscription/configuration/ApplicationConfig.java
+++ src/main/java/com/gc/services/subscription/configuration/ApplicationConfig.java
@@ -40,6 +40,6 @@ public class ApplicationConfig {
     @Bean
     public UserDetailsService userDetailService() {
         return username -> userRepository.findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("User not fournd"));
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
     }
 }
